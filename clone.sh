f77f12eeba3d316986aa1df5b0bc992cda45c403
Sam <s@sammagee.me>
2020-06-12 23:28:57 -0500

    add clone.sh, add fresh.sh for fresh installs

---
+++ clone.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+echo "Cloning repositories..."
+
+CODE=$HOME/Code
---
+++ fresh.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+echo "Setting up your Mac..."
+
+# Check for Homebrew and install if we don't have it
+if test ! $(which brew); then
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Update Homebrew recipes
+brew update
+
+# Install all our dependencies with bundle (See Brewfile)
+brew tap homebrew/bundle
+brew bundle
+
+# Set default MySQL root password and auth type.
+mysql -u root -e "ALTER USER root@localhost IDENTIFIED WITH mysql_native_password BY 'secret'; FLUSH PRIVILEGES;"
+
+# Install PHP extensions with PECL
+pecl install memcached imagick
+
+# Install global Composer packages
+/usr/local/bin/composer global require laravel/installer laravel/valet
+
+# Install Laravel Valet
+$HOME/.composer/vendor/bin/valet install
+
+# Create a Code directory
+mkdir $HOME/Code
+
+# Create sites subdirectories
+mkdir $HOME/Code/app
+mkdir $HOME/Code/misc
+mkdir $HOME/Code/oss
+mkdir $HOME/Code/pkg
+mkdir $HOME/Code/web
+
+# Clone Github repositories
+# ./clone.sh
+
+# Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
+rm -rf $HOME/.zshrc
+ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
+
+# Symlink the Mackup config file to the home directory
+ln -s $HOME/.dotfiles/.mackup.cfg $HOME/.mackup.cfg
+
+# Set macOS preferences
+# We will run this last because this will reload the shell
+source .macos
